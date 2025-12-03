4b3c836b50e54c75b659c4fff452564905de19a5
chengzj2 <chengzj2@lenovo.com>
2025-12-03 15:57:08 +0800

    修复例结果错误错误描述;添加对执行log文件数量和路径名称描述;添加对文件数量判断

--- monitoring/actions/Tools_Log_monitor.py
+++ monitoring/actions/Tools_Log_monitor.py
@@ -3,13 +3,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from anyio import current_time
-
 if TYPE_CHECKING:  # pragma: no cover
     from ..patvs_monitor import Patvs_Fuction
-
-import re
-from pathlib import Path
 
 from .Log_jugement_monitor import Log_parse
 
@@ -40,6 +35,9 @@ def run(context: "Patvs_Fuction",
 
         log_files=[path for path in log_files if display_action.lower().replace('log','') in path.lower()]
         for index,path in enumerate(log_files):
+            context.log(
+                f"{display_action} 执行文件数量为 {len(log_files)}，已选择文件为：{path}"
+            )
             try:
                 test_result=log_parse_.folder_files(path)
                 open_files.append(path)
@@ -50,21 +48,34 @@ def run(context: "Patvs_Fuction",
                 )
                 pass_result.append(test_result[-1])
 
+                if count_time==target_count_int:
+                    context.log(
+                        f"{display_action}事件已提前满足目标次数 {target_count_int}，当前计数 {count_time}/{target_count_int}。"
+                    )
+                    break
+
             except Exception as file_error:
                 context.logger.error(f"无法打开{display_action}日志文件 {path}: {file_error}")
-                context.log(f"无法打开{display_action}日志文件 {path}: {file_error}")
+                # context.log(f"无法打开{display_action}日志文件 {path}: {file_error}")
+                context.log(f"无法打开{display_action}日志文件 {path} , 请选择正确的{display_action}日志文件")
+                return
+
+        if count_time<target_count_int:
+            context.log(
+                f"{display_action}事件不满足目标次数 {target_count_int}，当前计数 {count_time}/{target_count_int}。"
+            )
+            return
 
         if "FAIL" in pass_result or pass_result==[]:
             context.log(
-                f"{display_action}事件已提前满足目标次数 {target_count_int}，当前计数 {count_time}/{target_count_int}。"
+                f"{display_action}事件没有满足测试通过要求，当前状态为 FAIL。"
             )
+            return
         else:
-            context.action_complete.set()
-
-        if not open_files:
             context.log(
-                f"无法监控 {display_action}，{display_action}日志文件打开失败。"
+                f"{display_action}事件已满测试通过要求，当前状态为 PASS。"
             )
+            context.action_complete.set()
             return
 
     except Exception as error:  # pragma: no cover - 兼容异常场景
@@ -72,5 +83,7 @@ def run(context: "Patvs_Fuction",
             f"监控{display_action}事件出现异常: {error}"
         )
         context.log(f"监控{display_action} 事件出现异常: {error}")
+        return
+
 
 
