7c615e6e8b4054c53d4b29f3ba2786802f40d3f4
chengzj2 <chengzj2@lenovo.com>
2025-12-04 14:37:10 +0800

    完善信息识别;添加html文件判断

--- monitoring/actions/Tools_Log_monitor.py
+++ monitoring/actions/Tools_Log_monitor.py
@@ -2,17 +2,16 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-
+from pathlib import Path
 if TYPE_CHECKING:  # pragma: no cover
     from ..patvs_monitor import Patvs_Fuction
 
-from .Log_jugement_monitor import Log_parse
+from .Log_jugement_monitor import LogParser,HtmlParser
 
 def run(context: "Patvs_Fuction",
         target_count: float,
         display_action: str | None = None) -> None:
 
-    log_parse_=Log_parse(context,display_action)
     open_files: list[tuple[str, object]] = []
     count_time=0
     pass_result=[]
@@ -32,14 +31,19 @@ def run(context: "Patvs_Fuction",
                 f"未选择任何日志文件，无法监控 {display_action}。"
             )
             return
-
         log_files=[path for path in log_files if display_action.lower().replace('log','') in path.lower()]
+        context.log(
+            f"{display_action} 执行文件数量为 {len(log_files)}"
+        )
         for index,path in enumerate(log_files):
             context.log(
-                f"{display_action} 执行文件数量为 {len(log_files)}，已选择文件为：{path}"
+                f"已选择第{index+1}个文件为：{Path(path).name}"
             )
             try:
-                test_result=log_parse_.folder_files(path)
+                if path.endswith('.html'):
+                    test_result=HtmlParser(context).finder_html_data(path)
+                else:
+                    test_result=LogParser(context,display_action).parse_log_file(path)
                 open_files.append(path)
                 count_time=index+1
 
@@ -47,7 +51,6 @@ def run(context: "Patvs_Fuction",
                     target_count, count_time, action_key=display_action.lower()
                 )
                 pass_result.append(test_result[-1])
-
                 if count_time==target_count_int:
                     context.log(
                         f"{display_action}事件已提前满足目标次数 {target_count_int}，当前计数 {count_time}/{target_count_int}。"
@@ -56,7 +59,6 @@ def run(context: "Patvs_Fuction",
 
             except Exception as file_error:
                 context.logger.error(f"无法打开{display_action}日志文件 {path}: {file_error}")
-                # context.log(f"无法打开{display_action}日志文件 {path}: {file_error}")
                 context.log(f"无法打开{display_action}日志文件 {path} , 请选择正确的{display_action}日志文件")
                 return
 
